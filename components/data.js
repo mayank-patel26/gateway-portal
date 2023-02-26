576995d65b026ed5ef32b78567e50e143591cd47
Mayank Patel <mayank509.mp@gmail.com>
2023-02-26 20:14:59 +0530

    CSS bugs fixes

--- components/data.js
+++ components/data.js
@@ -1,13 +1,13 @@
 export const data = [
     {name:"Student Profile Portal", link:"https://djsce-it-student-portal.netlify.app/", colorbg: "#361264", key: 1},
-    {name:"Placement System", link:"https://placement-portal-test.netlify.app/", colorbg: '#006E97', key: 2},
+    {name:"Placement Test Portal", link:"https://placement-portal-test.netlify.app/", colorbg: '#006E97', key: 2},
     {name:"Departmental Library", link:"", colorbg: '#BB5513', key: 3},
     {name:"Feedback System", link:"", colorbg: "#338036", key: 4},
     // {name:"", link:""},
 ]
 
 export const promoData = [
-    {logo: "/tele.png", key: 1, link: "https://www.instagram.com/djsce.official/"},
+    {logo: "/tele.png", key: 1, link: "https://www.instagram.com/djsce.it/"},
     {logo: "/insta.png", key: 2, link: "https://www.instagram.com/djsce.official/"},
     {logo: "/fb.png", key: 3, link: "https://www.facebook.com/djsce.official/"},
     {logo: "linkedIn.png", key: 4, link: "https://in.linkedin.com/school/djsce/"},
